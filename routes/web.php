d6ed9f3bb2f10e83a90ce60ff4b2bdd13e02c6f1
名前 <>
2021-01-22 13:31:04 +0000

    Routing2

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('caffe/create', 'Admin\CaffeController@add');
+});
